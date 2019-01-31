5c384c661cc41b68e709616c8fc8c2927f91a0c6
Giacomo <giacomobuzzitta@libero.it>
2019-01-31 16:59:36 +0100

    aggiunta menu

--- app/src/main/java/com/example/depeat/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/depeat/ui/activities/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.depeat.ui.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.depeat.R;
 
@@ -11,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater  = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.login_menu){
+
+            startActivity(new Intent(this,LoginActivity.class));
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
